10a48da5aa49912069e68b628003108e33ac4d39
linchaoqun <lchaoqun@sina.cn>
2023-05-19 14:45:39 +0800

    更新

--- js-test/sort.js
+++ js-test/sort.js
@@ -3,10 +3,19 @@ let path = require('path');
 let fs = require('fs');
 let readXlsxFile = require('read-excel-file/node');
 let base = require('../public/base.json');
-
+let axios = require('axios');
 
 async function main(){
-
+  for(let item of base){
+    let total = _.reduce(item.history, (sum, n)=> {
+      return sum + _.floor(n.value * n.count);
+    }, 0);
+    item.total = total;
+  }
+  base = _.sortBy(base, item=>{
+    return -item.total;
+  });
+  console.log(base);
   fs.writeFileSync(path.resolve(__dirname, '../public/base.json'), JSON.stringify(base, '', 2));
   fs.writeFileSync(path.resolve(__dirname, '../dist/base.json'), JSON.stringify(base, '', 2));
 }
